11926043be8abc5cd6622b1b55b615a8ae3af86f
Mark Wilde <markwilde29@gmail.com>
2019-11-17 15:13:15 +0000

    RAdded and wired the TodoItem.vue component.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Todos />
+    <Todos v-bind:todos="todos"/>
     <img alt="Vue logo" src="./assets/logo.png" />
   </div>
 </template>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="todo-item">
+    <p>{{todo.title}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"]
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: #ccc dotted;
+}
+
+.is-complete {
+  text-decoration: line-through;
+}
+
+.del {
+  background: #ff0000;
+  color: #fff;
+  border: none;
+  padding: 5px 9px;
+  border-radius: pointer;
+  float: right;
+}
+</style>
